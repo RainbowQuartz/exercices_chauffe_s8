90500fe6bdd79b5f9faa4a057dc516d2259d0baa
Léona Chevrel <chevrel.leona@gmail.com>
2018-08-20 10:58:08 +0200

    pushing

---
+++ exercice_chauffe.js
@@ -0,0 +1,13 @@
+function myMax(array){
+  return Math.max.apply(Math, array);
+}
+
+
+function vowelCount(string){
+  var m = str.match(/[aeiou]/gi);
+  return m === null ? 0 : m.length;
+}
+
+function reverse(string){
+  return s.split("").reverse().join("");
+}
